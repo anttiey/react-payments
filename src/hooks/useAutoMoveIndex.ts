8f6f2afdb0969bd7c190363cdb0ba04c8feda286
anttiey <hanuuny@gmail.com>
2024-05-09 15:59:20 +0900

    refactor(useAutoMoveIndex): 불필요한 useEffect 제거

--- src/hooks/useAutoMoveIndex.ts
+++ src/hooks/useAutoMoveIndex.ts
@@ -1,16 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const useAutoMoveIndex = (defaultIndex: number, validationList: boolean[]) => {
   const [index, setIndex] = useState(defaultIndex);
 
-  useEffect(() => {
-    if (validationList.some(Boolean)) {
-      const nextIndex = defaultIndex + validationList.filter(Boolean).length;
-      handleIndex(nextIndex);
-    }
-  }, [validationList]);
-
   const handleIndex = (nextIndex: number) => nextIndex > index && setIndex(nextIndex);
+
+  if (validationList.some(Boolean)) {
+    const nextIndex = defaultIndex + validationList.filter(Boolean).length;
+    handleIndex(nextIndex);
+  }
 
   return { moveIndex: index };
 };
